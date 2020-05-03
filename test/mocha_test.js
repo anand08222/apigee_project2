f18aa9f6e55600b01fe8c2a011a6be5ed10affa4
Anand Tiwari <64299402+anand08222@users.noreply.github.com>
2020-05-03 17:15:34 +0530

    Update mocha_test.js

--- test/mocha_test.js
+++ test/mocha_test.js
@@ -1,68 +1,13 @@
-var assert = require('assert'),
-    should = require('should'),
-    expect = require('chai').expect;
+const assert = require('chai').aasert;
 
-
-var answer;
-var user = {
-    name: 'tj',
-    pets: ['tobi', 'loki', 'jane', 'bandit']
-};
-
-
-
-var foo = 'bar',
-    beverages = { tea: ['chai', 'matcha', 'oolong'] };
-
-describe('Test Suite 1', function() {
-    describe('Scenario 1', function() {
-        it('Array index check', function() {
-            assert.equal(-1, [1, 2, 3].indexOf(4));
-        });
-
-        it('Array property check', function() {
-            user.should.have.property('name', 'tj');
-            user.should.have.property('pets').with.lengthOf(4);
-        });
-
-        it('Foo is a string', function() {
-            expect(foo).to.be.a('string');
-        });
-    });
-
-    describe('Scenario 2', function() {
-        it('Bar Equals', function() {
-            expect(foo).to.equal('bar');
-
-        });
-
-        it('Length Equals', function() {
-            expect(foo).to.have.lengthOf(3);
-
-        });
-
-        it('Property Equals', function() {
-            expect(beverages).to.have.property('tea').with.lengthOf(3);
-        });
-    });
-
-
-});
-
-
-describe('Test Suite 2', function() {
-    before(function() {
-        answer = 43;
-    });
-
-    describe('Scenario 1', function() {
-
-        it('Equals Check', function() {
-            expect(answer).to.equal(43);
-        })
-
-        it('Expected Check', function() {
-            expect(answer, 'topic [answer]').to.equal(43);
-        })
-    });
+describe('Field test for killer robot', function(){
+	
+	let num = 6;
+	let cum = 'dsjvhwshvehw';
+	it('Check that num is numeric',function(){
+		assert.isNumber(num);
+	});
+	it('check that cum is string',function(){
+		assert.isString(cum);
+	});
 });
